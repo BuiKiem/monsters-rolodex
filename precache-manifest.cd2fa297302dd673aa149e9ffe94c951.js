44aa72c8d22dbd35c95d4019e8d6003fa0ad2cc3
Bui Kiem <buivantrung1105@gmail.com>
2019-08-22 18:53:18 +0700

    Updates

---
+++ precache-manifest.cd2fa297302dd673aa149e9ffe94c951.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7756802f8e2985bd3b565ae7a559349",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4b465ee407b82976dc95",
+    "url": "/monsters-rolodex/static/css/main.c3679f33.chunk.css"
+  },
+  {
+    "revision": "62db5322e3274305e287",
+    "url": "/monsters-rolodex/static/js/2.0f95dd00.chunk.js"
+  },
+  {
+    "revision": "4b465ee407b82976dc95",
+    "url": "/monsters-rolodex/static/js/main.3ef195c5.chunk.js"
+  },
+  {
+    "revision": "9ea9c29140c1f80c8044",
+    "url": "/monsters-rolodex/static/js/runtime~main.3810dc4c.js"
+  }
+]);
